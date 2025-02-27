81f01211daafd0e79000858df214ddb7132c5b0b
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-02-27 12:41:18 +0530

    feat: Add list view settings for Payment Processor Setting

---
+++ payments_processor/payments_processor/doctype/payment_processor_setting/payment_processor_setting_list.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2025, Resilient Tech and contributors
+// For license information, please see license.txt
+
+frappe.listview_settings["Payment Processor Setting"] = {
+	hide_name_column: true,
+	hide_name_filter: true,
+};
