435fd3cc6ce287b7962038ee18e57e53e9e65818
Smit Vora <smitvora203@gmail.com>
2025-02-28 15:09:11 +0530

    fix: creation of payment entry in working state

--- payments_processor/hooks.py
+++ payments_processor/hooks.py
@@ -7,3 +7,12 @@ app_license = "GNU General Public License (v3)"
 
 after_install = "payments_processor.install.after_install"
 before_uninstall = "payments_processor.uninstall.before_uninstall"
+
+scheduler_events = {
+    "cron": {
+        # 1 pm daily
+        "0 13 * * *": [
+            "payments_processor.payments_processor.utils.automation.autocreate_payment_entry"
+        ]
+    }
+}
--- payments_processor/setup.py
+++ payments_processor/setup.py
@@ -7,6 +7,9 @@ from frappe.custom.doctype.custom_field.custom_field import (
 from payments_processor.payments_processor.constants.custom_fields import (
     CUSTOM_FIELDS,
 )
+from payments_processor.payments_processor.constants.email_template import (
+    EMAIL_TEMPLATES,
+)
 from payments_processor.payments_processor.constants.property_setters import (
     PROPERTY_SETTERS,
 )
@@ -15,6 +18,7 @@ from payments_processor.payments_processor.setup import (
     delete_custom_fields,
     delete_property_setters,
     delete_roles_and_permissions,
+    make_email_templates,
     make_roles_and_permissions,
 )
 
@@ -30,6 +34,9 @@ def setup_customizations():
     click.secho("Creating Property Setters...", fg="blue")
     create_property_setters()
 
+    click.secho("Creating Email Templates...", fg="blue")
+    create_email_templates()
+
 
 # Note: separate functions are required to use in patches
 def create_roles_and_permissions():
@@ -43,6 +50,10 @@ def create_custom_fields():
 def create_property_setters():
     for property_setter in PROPERTY_SETTERS:
         frappe.make_property_setter(property_setter)
+
+
+def create_email_templates():
+    make_email_templates(EMAIL_TEMPLATES)
 
 
 ################### Before Uninstall ###################
