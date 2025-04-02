da8faf5de058dd5721be10f4313b13ee84ddbee5
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-04-02 17:10:26 +0530

    fix: correct typo in comment for configurability in hooks.py

--- payments_processor/hooks.py
+++ payments_processor/hooks.py
@@ -8,7 +8,7 @@ app_license = "GNU General Public License (v3)"
 after_install = "payments_processor.install.after_install"
 before_uninstall = "payments_processor.uninstall.before_uninstall"
 
-# TODO: Make this comfigurable
+# TODO: Make this configurable
 scheduler_events = {
     "all": [
         "payments_processor.payments_processor.utils.automation.autocreate_payment_entry"
