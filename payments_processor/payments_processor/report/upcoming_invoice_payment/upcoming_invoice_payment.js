b279fd5da6f48c0412c516fbe1e342a173c52a97
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-04-18 12:10:00 +0530

    chore: covert indentation to spaces

--- payments_processor/payments_processor/report/upcoming_invoice_payment/upcoming_invoice_payment.js
+++ payments_processor/payments_processor/report/upcoming_invoice_payment/upcoming_invoice_payment.js
@@ -2,21 +2,21 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Upcoming Invoice Payment"] = {
-	filters: [
-		{
-			fieldname: "company",
-			label: __("Company"),
-			fieldtype: "Link",
-			options: "Company",
-			default: frappe.defaults.get_user_default("Company"),
-			reqd: 1,
-		},
-		{
-			fieldname: "payment_date",
-			label: __("Payment Upto Date"),
-			fieldtype: "Date",
-			reqd: 1,
-			default: frappe.datetime.add_days(frappe.datetime.now_date(), 1),
-		},
-	],
+    filters: [
+        {
+            fieldname: "company",
+            label: __("Company"),
+            fieldtype: "Link",
+            options: "Company",
+            default: frappe.defaults.get_user_default("Company"),
+            reqd: 1,
+        },
+        {
+            fieldname: "payment_date",
+            label: __("Payment Upto Date"),
+            fieldtype: "Date",
+            reqd: 1,
+            default: frappe.datetime.add_days(frappe.datetime.now_date(), 1),
+        },
+    ],
 };
